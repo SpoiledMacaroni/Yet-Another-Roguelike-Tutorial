e47689e756ca8a8b26ef1670f193820216144c91
SpoiledMacaroni <60438998+SpoiledMacaroni@users.noreply.github.com>
2024-02-21 10:30:19 -0500

    Add Main Menu, Save and Load function Added main menu and popup, saving and loading functionality

--- input_handlers.py
+++ input_handlers.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import os
+
 from typing import Callable, Optional, Tuple, TYPE_CHECKING, Union
 
 import tcod.event
@@ -83,6 +85,31 @@ class BaseEventHandler(tcod.event.EventDispatch[ActionOrHandler]):
   
   def ev_quit(self, event:tcod.event.Quit) -> Optional[Action]:
     raise SystemExit()
+
+class PopupMessage(BaseEventHandler):
+  """Display a popup text window."""
+  def __init__(self, parent_handler: BaseEventHandler, text: str):
+    self.parent = parent_handler
+    self.text = text
+  
+  def on_render(self, console: tcod.console.Console) -> None:
+    """Render the parent and dim the result, then print the message on top."""
+    self.parent.on_render(console)
+    console.rgb["fg"] //= 8
+    console.rgb["bg"] //= 8
+
+    console.print(
+      console.width//2,
+      console.height//2,
+      self.text,
+      fg=color.white,
+      bg=color.black,
+      alignment=libtcodpy.CENTER,
+    )
+  
+  def ev_keydown(self, event: tcod.event.KeyDown) -> Optional[BaseEventHandler]:
+    """Any key returns to the parent handler."""
+    return self.parent
 
 class EventHandler(BaseEventHandler):
   def __init__(self, engine: Engine):
@@ -335,7 +362,6 @@ class AreaRangedAttackHandler(SelectIndexHandler):
   def on_index_selected(self, x: int, y: int) -> Optional[Action]:
     return self.callback((x, y))
 
-
 class MainGameEventHandler(EventHandler):  
   def ev_keydown(self, event: tcod.event.KeyDown) -> Optional[ActionOrHandler]:
     action: Optional[Action] = None
@@ -371,9 +397,18 @@ class MainGameEventHandler(EventHandler):
     return action
 
 class GameOverEventHandler(EventHandler):
-  def ev_keydown(self, event: tcod.event.KeyDown) -> Optional[Action]:
+  def on_quit(self) -> None:
+    """Handle exiting out of a finished game."""
+    if os.path.exists("savegame.sav"):
+      os.remove("savegame.sav") # Deletes the active save file.
+    raise exceptions.QuitWithoutSaving() # Avoid saving a finished game.
+  
+  def ev_quit(self, event: tcod.event.Quit) -> None:
+    self.on_quit()
+
+  def ev_keydown(self, event: tcod.event.KeyDown) -> None:
     if event.sym == tcod.event.KeySym.ESCAPE:
-      raise SystemExit()
+      self.on_quit()
   
 CURSOR_Y_KEYS = {
   tcod.event.KeySym.UP: -1,
