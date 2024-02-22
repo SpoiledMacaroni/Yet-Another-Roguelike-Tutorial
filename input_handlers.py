6264a88099c7423f384898c30dd6b0e197e12a0c
SpoiledMacaroni <60438998+SpoiledMacaroni@users.noreply.github.com>
2024-02-21 20:13:54 -0500

    implemented levelup system player now gains xp as they kill mobiles, levelling up over time.

--- input_handlers.py
+++ input_handlers.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import os
 
 from typing import Callable, Optional, Tuple, TYPE_CHECKING, Union
+from tcod.console import Console
 
 import tcod.event
 from tcod import libtcodpy
@@ -125,6 +126,8 @@ class EventHandler(BaseEventHandler):
       if not self.engine.player.is_alive:
         #The player was killed sometime during or after the action.
         return GameOverEventHandler(self.engine)
+      elif self.engine.player.level.requires_level_up:
+        return LevelUpEventHandler(self.engine)
       return MainGameEventHandler(self.engine) #return to the main handler.
     return self
   
@@ -180,6 +183,108 @@ class AskUserEventHandler(EventHandler):
     
     By default this returns to the main event handler."""
     return MainGameEventHandler(self.engine)
+
+class CharacterScreenEventHandler(AskUserEventHandler):
+  TITLE = "Character Information"
+
+  def on_render(self, console: tcod.console.Console) -> None:
+    super().on_render(console)
+
+    if self.engine.player.x <= 30:
+      x = 40
+    else: 
+      x = 0
+    
+    y = 0
+
+    width = len(self.TITLE) + 4
+
+    console.draw_frame(
+      x=x,
+      y=y,
+      width=width,
+      height=7,
+      title=self.TITLE,
+      clear=True,
+      fg=(255,255,255),
+      bg=(0,0,0),
+    )
+
+    console.print(
+        x=x + 1, y=y + 1, string=f"Level: {self.engine.player.level.current_level}"
+    )
+    console.print(
+        x=x + 1, y=y + 2, string=f"XP: {self.engine.player.level.current_xp}"
+    )
+    console.print(
+        x=x + 1,
+        y=y + 3,
+        string=f"XP for next Level: {self.engine.player.level.experience_to_next_level}",
+    )
+
+    console.print(
+        x=x + 1, y=y + 4, string=f"Attack: {self.engine.player.fighter.power}"
+    )
+    console.print(
+        x=x + 1, y=y + 5, string=f"Defense: {self.engine.player.fighter.defense}"
+    )
+
+class LevelUpEventHandler(AskUserEventHandler):
+  TITLE = "Level Up"
+
+  def on_render(self, console: tcod.console.Console) -> None:
+    super().on_render(console)
+
+    if self.engine.player.x <= 30:
+      x = 40
+    else:
+      x = 0
+    
+    console.print(x=x+1, y=1, string="Congratulations! You level up!")
+    console(x=x+1, y=2, string="Select an attribute to increase.")
+
+    console.print(
+      x=x+1,
+      y=4,
+      string=f"a) Constitution (+20 HP, from {self.engine.player.fighter.max_hp})",
+    )
+
+    console.print(
+      x=x+1,
+      y=5,
+      string=f"a) Strength (+1 attack, from {self.engine.player.fighter.power})",
+    )
+
+    console.print(
+      x=x+1,
+      y=6,
+      string=f"a) Agility (+1 defense, from {self.engine.player.fighter.defense})",
+    )
+  
+  def ev_keydown(self, event: tcod.event.KeyDown) -> Optional[ActionOrHandler]:
+    player=self.engine.player
+    key= event.sym
+    index = key - tcod.event.KeySym.a
+
+    if 0 <= index <= 2:
+      if index == 0:
+        player.level.increase_max_hp()
+      elif index == 1:
+        player.level.increase_power()
+      else: 
+        player.level.increase_defense()
+    else:
+      self.engine.message_log.add_message("Invalid entry.", color.invalid)
+
+      return None
+        
+    return super().ev_keydown(event)
+  
+  def ev_mousebuttondown(
+      self, event: tcod.event.MouseButtonDown
+      ) -> Optional[ActionOrHandler]:
+    """Don't permit the player to exit the menu per normal"""
+    return None
 
 class InventoryEventHandler(AskUserEventHandler):
   """This handler lets the user select an item.
@@ -396,6 +501,8 @@ class MainGameEventHandler(EventHandler):
       return InventoryActivateHandler(self.engine)
     elif key == tcod.event.KeySym.d:
       return InventoryDropHandler(self.engine)
+    elif key == tcod.event.KeySym.c:
+      return CharacterScreenEventHandler(self.engine)
     elif key == tcod.event.KeySym.SLASH:
       return LookHandler(self.engine)
 
